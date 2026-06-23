bb995ef3cde7df143de224e762bb76fc8530dd34
Alexandra Militan Mocanu <alexandra.militan@gmail.com>
2026-06-23 16:31:05 +0200

    added some tests (prints to terminal) to understand the issues encountered when animating the scene

--- src/PlasticConstraint/BoxLagrangianConstraint.cpp
+++ src/PlasticConstraint/BoxLagrangianConstraint.cpp
@@ -12,9 +12,12 @@ using namespace sofa::defaulttype;
 void registerBoxLagrangianConstraint(sofa::core::ObjectFactory* factory)
 {
     factory->registerObjects(sofa::core::ObjectRegistrationData("Lagrangian-based constraint forcing a 1D DoF to be  inside a given range.")
+        //.add< BoxLagrangianConstraint<Vec3Types> >()); //pour test avec vec3
         .add< BoxLagrangianConstraint<Vec6Types> >());
+
 }
 
+//template class PLASTICCONSTRAINT_API BoxLagrangianConstraint<Vec3Types>; //test avec vec3
 template class PLASTICCONSTRAINT_API BoxLagrangianConstraint<Vec6Types>;
 
 } //namespace sofa::component::constraint::lagrangian::model
